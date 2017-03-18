5371ebd3fc7c3d871d45daeed3a09588f7d4a5b2
Gordon Pimblott <g.pimblott@kainos.com>
2017-03-18 16:29:22 +0000

    remove SSL enforcement for digidad domain

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var session = require('express-session');
-var express_enforces_ssl = require('express-enforces-ssl');
 
 var basicAuth = require('./utils/basicAuth.js');
 
@@ -119,7 +118,6 @@ var SprintRunner = function () {
     if (self.app.get('env') == 'production') {
       self.app.enable('trust proxy', 1); // trusts first proxy - Heroku load balancer
       console.log("In production mode");
-      self.app.use(express_enforces_ssl());
       sess.cookie.secure = true;
     }
 
